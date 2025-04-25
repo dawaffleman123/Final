55d7399d977716a74060c2503d954714be1aa20b
maggie <petermaggie28@gmail.com>
2025-04-24 22:42:45 -0400

    add GUIFrame class for user input of IP address and ports

---
+++ com/scanner/GUIFrame.java
@@ -0,0 +1,43 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GUIFrame extends JFrame {
+
+    public static final int TEXTAREA_ROWS = 8;
+    public static final int TEXTAREA_COLUMNS = 20;
+
+    public GUIFrame()
+    {
+        JTextField IPaddress = new JTextField();
+        JTextField firstPort = new JTextField();
+        JTextField secondPort = new JTextField();
+
+        JPanel northPanel = new JPanel();
+        northPanel.setLayout(new GridLayout(2, 3));
+        northPanel.add(new JLabel("Insert Computer IP Address: ", SwingConstants.RIGHT));
+        northPanel.add(IPaddress);
+        northPanel.add(new JLabel("Specify Port (will scan all by default, or specify a port or range)", SwingConstants.RIGHT));
+        northPanel.add(firstPort);
+        northPanel.add(secondPort);
+
+        add(northPanel, BorderLayout.NORTH);
+
+        JTextArea textArea = new JTextArea(TEXTAREA_ROWS, TEXTAREA_COLUMNS);
+        JScrollPane scrollPane = new JScrollPane(textArea);
+
+        add(scrollPane, BorderLayout.CENTER);
+
+        // add button to append text into the text area
+
+        JPanel southPanel = new JPanel();
+
+        JButton insertButton = new JButton("Insert");
+        southPanel.add(insertButton);
+        insertButton.addActionListener(event ->
+                textArea.append("IP address entered: " + IPaddress.getText() + " : "
+                        + new String(passwordField.getPassword()) + "\n"));
+
+        add(southPanel, BorderLayout.SOUTH);
+        pack();
+    }
+}
